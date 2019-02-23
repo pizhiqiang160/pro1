3b18002948cc7b3affdfd26596bd7b1cb40d06c6
pizhiqiang160 <328956221@qq.com>
2019-02-23 17:34:59 +0800

    修改更新

--- shell1/abcd1234.sh
+++ shell1/abcd1234.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-a="皮志强 朱冬冬 黄科 陈志红 李团兵 姜一炜 宋贺 伍小龙 尹杨陈 杨荣 刘初杰 李偃文"
-#d=(0 0 0 0 0 0 0 0 0 0 0 0)
+#trap "" INT 
+#g=$1
+#g=${g:-姜一炜}  
+#d=(0 0 0 0 0 0 0 0 0 0 0 0) 
+
+#for j in {1..10000}  
+#do  
+a="皮志强 朱冬冬 黄科 陈志红 李团兵 宋贺 姜一炜 伍小龙 尹杨陈 杨荣 刘初杰 李偃文"
+
 #c=0
 #for i in ${a[@]}
+
 for i in `seq 12`
 do
 let k=13-$i
@@ -11,8 +19,19 @@ let sj=$RANDOM*$k/32768+1
 b=$(echo $a |awk 'BEGIN{var1="'"$sj"'"}{print $var1}')
 echo -e "${i}\t-->$b"
 a=${a/$b/}
-#sleep 2
+sleep 5
+#[ $b ==  $g ] && let d[i-1]++
+
 done
+
+#显示进度信息
+#jindu=`echo "scale=2;$j/100 " | bc `
+#echo -en "\r$jindu%"
+#echo scale=4 $j/10000.00 | bc
+#done
+
+#echo "${d[*]}"
+
 
 klll000(){
 while [ $c -lt 12 ] 
--- shell1/sqlauto.sh
+++ shell1/sqlauto.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
+ip1=`ifconfig eth0 |sed -n '2p' |awk 'OFS=","{print $2,$4}'`
+nmcli connection modify eth0 ipv4.method manual ipv4.addresses $ip1 connection.autoconnect yes
+nmcli connection up eth0
+hostname mysql
+hostname >/etc/hostname
+echo "ip设置完成"
 
+a()
+{
 #无密码验证登陆，人机交互
 list=`seq 52 56`
 id=`ifconfig eth0|sed -n '2p' |awk '{print $2}' |awk -F. '{print $NF}'`
 list2=${list/$id/}
 ip1=192.168.4.
-ssh-keygen -N '' -f /root/.ssh/id_rsa
+
+[ ! -f '/root/.ssh/id_rsa' ] && ssh-keygen -N '' -f /root/.ssh/id_rsa
 for i in $list2
 do
 ssh-copy-id $ip1$i:
 done
+>/tmp/host.txt
+for i in {51..56}
+do
+echo "192.168.4.$i" >>/tmp/host.txt
+done
+if rpm -qa pssh*
+wget http://176.52.8.204/pssh-2.3.1-5.el7.noarch.rpm -O /tmp/pssh-2.3.1-5.el7.noarch.rpm 
 
-
-
-cd /var/lib/mysql
-rm -rf *.0000* *.index *.info
+pssh -i -p 2 -h /tmp/host.txt 'rm -rf /var/lib/mysqli/{*.0000*,*.index,*.info}
 systemctl restart mysqld
+'
+
 stat -c %G /var/lib/mysql
-
-
 ##########             以下为SQL执行命令
 # 第一台主库执行（192.168.4.52）
 #grant replication slave on *.* to lx@'%' identdified by '123456';
 # 其他从库执行 （192.168.4.53-56）
 #change master to master_host="192.168.4.52",master_user="lx",master_password="123456",master_log_file="s1.000001",master_log_pos=435;
+
+
+}
