bee4364b0a7859a86aa299c161e988d293999740
pizhiqiang160 <328956221@qq.com>
2019-03-19 08:43:36 +0800

    abcd1234

--- shell1/abcd1234.sh
+++ shell1/abcd1234.sh
@@ -1,44 +1,12 @@
 #!/bin/bash
-#trap "" INT 
-#g=$1
-#g=${g:-姜一炜}  
-#d=(0 0 0 0 0 0 0 0 0 0 0 0) 
-
-#for j in {1..10000}  
-#do  
-a="皮志强 朱冬冬 黄科 陈志红 李团兵 宋贺 姜一炜 伍小龙 尹杨陈 杨荣 刘初杰 李偃文"
-
-#c=0
-#for i in ${a[@]}
-
-for i in `seq 12`
+a="皮志强 朱冬冬 黄科 陈志红 李团兵 宋贺 姜一炜 伍小龙 尹杨陈 杨荣 刘秋杰 李偃文"
+num1=`echo "$a" |awk '{print NF}'`  
+for i in `seq $num1`
 do
-let k=13-$i
+let k=${num1}-$i+1
 let sj=$RANDOM*$k/32768+1
-
-b=$(echo $a |awk 'BEGIN{var1="'"$sj"'"}{print $var1}')
+b=$(echo $a |awk -v var1=$sj '{print $var1}')
 echo -e "${i}\t-->$b"
 a=${a/$b/}
 sleep 5
-#[ $b ==  $g ] && let d[i-1]++
-
 done
-
-#显示进度信息
-#jindu=`echo "scale=2;$j/100 " | bc `
-#echo -en "\r$jindu%"
-#echo scale=4 $j/10000.00 | bc
-#done
-
-#echo "${d[*]}"
-
-
-klll000(){
-while [ $c -lt 12 ] 
-do
-i=$[RANDOM*12/32768]
-[ ${d[i]} -gt 0 ]&& continue ||let d[$i]++
-echo 第$((++b))个为${a[i]} 
-let c++
-done
-}
--- shell1/sqlauto.sh
+++ shell1/sqlauto.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+
+
+setip1(){
 ip1=`ifconfig eth0 |sed -n '2p' |awk 'OFS=","{print $2,$4}'`
 nmcli connection modify eth0 ipv4.method manual ipv4.addresses $ip1 connection.autoconnect yes
 nmcli connection up eth0
 hostname mysql
 hostname >/etc/hostname
 echo "ip设置完成"
+}
 
 a()
 {
@@ -24,12 +29,10 @@ for i in {51..56}
 do
 echo "192.168.4.$i" >>/tmp/host.txt
 done
-if rpm -qa pssh*
+if rpm -qa pssh* ; then
 wget http://176.52.8.204/pssh-2.3.1-5.el7.noarch.rpm -O /tmp/pssh-2.3.1-5.el7.noarch.rpm 
-
-pssh -i -p 2 -h /tmp/host.txt 'rm -rf /var/lib/mysqli/{*.0000*,*.index,*.info}
-systemctl restart mysqld
-'
+fi
+pssh -i -p 2 -h /tmp/host.txt 'rm -rf /var/lib/mysql/{*.0000*,*.index,*.info};systemctl restart mysqld'
 
 stat -c %G /var/lib/mysql
 ##########             以下为SQL执行命令
@@ -37,6 +40,39 @@ stat -c %G /var/lib/mysql
 #grant replication slave on *.* to lx@'%' identdified by '123456';
 # 其他从库执行 （192.168.4.53-56）
 #change master to master_host="192.168.4.52",master_user="lx",master_password="123456",master_log_file="s1.000001",master_log_pos=435;
-
-
 }
+
+#redis
+redis1(){
+systemctl disable mysqld
+systemctl stop mysqld
+rpm -q gcc ||yum -y install gcc
+if [ -f '/root/redis-4.0.8.tar.gz'  ] ;then
+tar -xf /root/redis-4.0.8.tar.gz
+cd redis-4.0.8
+make
+make install
+fi
+}
+
+	if [ $# -ne 0 ];then
+		for i in $*
+			do
+				$i
+			done
+	else 
+		redis1
+fi
+
+
+/root/redis-4.0.8/utils/install_server.sh <<EOF
+\n
+\n
+\n
+\n
+\n
+EOF
+
+
+
+echo 0000
